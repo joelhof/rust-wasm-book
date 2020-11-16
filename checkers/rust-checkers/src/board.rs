1cde5e0e5be1a8697c483d1c7b12a4190473064b
Joel Hofgren <joel.hofgren@squeed.com>
2020-11-16 23:30:59 +0100

    Add constructs for representing board squares and moves between squares.

--- checkers/rust-checkers/src/board.rs
+++ checkers/rust-checkers/src/board.rs
@@ -25,3 +25,60 @@ impl GamePiece {
         }
     }
 }
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Coordinate(pub usize, pub usize);
+
+impl Coordinate {
+    pub fn onBoard(self) -> bool {
+        let Coordinate(x,y) = self;
+        x <= 7 && y <= 7
+    }
+
+    pub fn jumpTargetsFrom(&self) -> impl Iterator<Item = Coordinate> {
+        let mut jumps = Vec::new();
+        let Coordinate(x, y) = *self;
+        if y >= 2 {
+            jumps.push(Coordinate(x + 2, y - 2));
+        }
+        jumps.push(Coordinate(x + 2, y + 2));
+        if x >= 2 && y >= 2 {
+            jumps.push(Coordinate(x - 2, y - 2));
+        }
+        if x >= 2 {
+            jumps.push(Coordinate(x - 2, y + 2));
+        }
+        jumps.into_iter()
+    }
+
+    pub fn moveTargetsFrom(&self) -> impl Iterator<Item = Coordinate> {
+        let mut moves = Vec::new();
+        let Coordinate(x, y) = *self;
+        if x >= 1 {
+            moves.push(Coordinate(x - 1, y + 1));
+        }
+        moves.push(Coordinate(x + 1, y + 1));
+        if x >= 1 && y >= 1 {
+            moves.push(Coordinate(x - 1, y - 1));
+        }
+        if y >= 1 {
+            moves.push(Coordinate(x + 1, y - 1));
+        }
+        moves.into_iter();
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Move {
+    pub from: Coordinate,
+    pub to: Coordinate
+}
+
+impl Move {
+    pub fn new(from: (usize, usize), to: (usize, usize)) -> Move {
+        Move {
+            from: Coordinate(from.0, from.1),
+            to: Coordinate(to.0, to.1)
+        }
+    }
+}
