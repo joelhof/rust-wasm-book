608ed64d44084eb788d986419ed3fddac496234f
Joel Hofgren <joel.hofgren@squeed.com>
2020-11-22 21:47:42 +0100

    There is something weird with the notification callbacks.
    They don't seem to be called in the loop.

--- checkers/rust-checkers/demo/index.js
+++ checkers/rust-checkers/demo/index.js
@@ -2,14 +2,12 @@ fetch('./rust_checkers.wasm')
     .then(response => response.arrayBuffer())
     .then(bytes => WebAssembly.instantiate(bytes, {
         env: {
-            notify_piececrowned: 
-                (x, y) => {
-                    console.log("A piece was crowned at ", [x, y]);
-                },
-            notify_piecemoved: 
-                (fromX, fromY, toX, toY) => {
-                    console.log("A piece moved from ", [fromX,fromY], "to", [toX,toY]);
-                }
+            notify_piececrowned: (x, y) => {
+                console.log("A piece was crowned at ", [x, y]);
+            },
+            notify_piecemoved: (fromX, fromY, toX, toY) => {
+                console.log("A piece moved from ", [fromX,fromY], "to", [toX,toY]);
+            }
         }
     })).then(results => {
         var white = 2;
@@ -29,11 +27,25 @@ fetch('./rust_checkers.wasm')
             console.log(...blackMoves[i], instance.exports.move_piece(...blackMoves[i]));
             console.log("piece at ", blackMoves[i].slice(2), instance.exports.get_piece(...blackMoves[i].slice(2)));
             console.log("current player is", instance.exports.get_current_turn());
-        console.log(...whiteMoves[i], instance.exports.move_piece(...whiteMoves[i]));
+            console.log(...whiteMoves[i], instance.exports.move_piece(...whiteMoves[i]));
             printBoard(instance);
         }
        
         console.log("turn owner is black", instance.exports.get_current_turn() == black);
+
+        console.log("At start, current turn is " +
+    instance.exports.get_current_turn());
+    let piece = instance.exports.get_piece(0, 7);
+    console.log("Piece at 0,7 is " + piece);
+
+    let res = instance.exports.move_piece(0, 5, 1, 4); // B
+    console.log("First move result: " + res);
+    console.log("Turn after move: " + instance.exports.get_current_turn());
+
+    let bad = instance.exports.move_piece(1, 4, 2, 3); // illegal move
+    console.log("Illegal move result: " + bad);
+    console.log("Turn after illegal move: " +
+    instance.exports.get_current_turn());
     }).catch(console.error);
 
 function printBoard(instance) {
