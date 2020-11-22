bf4d59b519deffa94de53cc1a82f2e47f544b965
Joel Hofgren <joel.hofgren@squeed.com>
2020-11-22 19:25:18 +0100

    Create global game engine instance.

--- checkers/rust-checkers/src/lib.rs
+++ checkers/rust-checkers/src/lib.rs
@@ -1,4 +1,17 @@
 #![allow(non_snake_case)]
+#[macro_use]
+extern crate lazy_static;
+extern crate mut_static;
+use board::{Coordinate, GamePiece, Move, PieceColor};
+use game::GameEngine;
+use mut_static::MutStatic;
+
+lazy_static! {
+    pub static ref GAME_ENGINE: MutStatic<GameEngine> = {
+        MutStatic::from(GameEngine::new())
+    };
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
