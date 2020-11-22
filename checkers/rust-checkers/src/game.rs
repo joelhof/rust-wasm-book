de00f59cf74ffbe886c7af940ae5bd2c50aaa2c0
Joel Hofgren <joel.hofgren@squeed.com>
2020-11-22 18:33:55 +0100

    Test shouldCrown from book source code.

--- checkers/rust-checkers/src/game.rs
+++ checkers/rust-checkers/src/game.rs
@@ -132,9 +132,9 @@ impl GameEngine {
     }
 
     fn validMove(&self, piece: &GamePiece, pos: &Coordinate, target: &Coordinate) -> bool {
-        let Coordinate(x1, y1) = *pos;
+        let Coordinate(_x1, y1) = *pos;
         let Coordinate(x2, y2) = *target;
-        if let Some(occupant) = self.board[x2][y2] {
+        if let Some(_occupant) = self.board[x2][y2] {
             return false;
         } else {
             if piece.crowned {
@@ -189,4 +189,264 @@ impl GameEngine {
         self.turnOwner = nextTurnOwner;
         self.moveCount = self.moveCount + 1;
     }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::super::board::{Coordinate, GamePiece, Move, PieceColor};
+    use super::GameEngine;
+
+    #[test]
+    fn should_crown() {
+        let engine = GameEngine::new();
+        let black = GamePiece::new(PieceColor::Black);
+        let res = engine.shouldCrown(black, Coordinate(3, 0));
+        assert!(res);
+        let res_nocrown = engine.shouldCrown(black, Coordinate(5, 2));
+        assert_eq!(res_nocrown, false);
+    }
+
+/*     #[test]
+    fn mut_crown() {
+        let mut engine = GameEngine::new();
+        engine.initialize_pieces();
+        let crowned = engine.crown_piece(Coordinate(1, 0));
+        assert!(crowned);
+        assert!(engine.is_crowned(Coordinate(1, 0)));
+    }
+
+    #[test]
+    fn advance_turn() {
+        let mut engine = GameEngine::new();
+        engine.advance_turn();
+        assert_eq!(engine.current_turn(), PieceColor::White);
+        engine.advance_turn();
+        assert_eq!(engine.current_turn(), PieceColor::Black);
+        assert_eq!(engine.move_count(), 2);
+    }
+
+    #[test]
+    fn move_targets() {
+        let c1 = Coordinate(0, 5);
+        let targets = c1.move_targets_from().collect::<Vec<Coordinate>>();
+        assert_eq!(targets, [Coordinate(1, 6), Coordinate(1, 4)]);
+
+        let c2 = Coordinate(1, 6);
+        let targets2 = c2.move_targets_from().collect::<Vec<Coordinate>>();
+        assert_eq!(
+            targets2,
+            [
+                Coordinate(0, 7),
+                Coordinate(2, 7),
+                Coordinate(2, 5),
+                Coordinate(0, 5)
+            ]
+        );
+
+        let c3 = Coordinate(2, 5);
+        let targets3 = c3.move_targets_from().collect::<Vec<Coordinate>>();
+        assert_eq!(
+            targets3,
+            [
+                Coordinate(1, 6),
+                Coordinate(3, 6),
+                Coordinate(3, 4),
+                Coordinate(1, 4)
+            ]
+        );
+    }
+
+    #[test]
+    fn valid_from() {
+        let c1 = Coordinate(0, 5);
+        let c2 = Coordinate(2, 5);
+
+        let mut engine = GameEngine::new();
+        engine.initialize_pieces();
+        let m1 = engine.valid_moves_from(c1);
+        let m2 = engine.valid_moves_from(c2);
+        assert_eq!(
+            m1,
+            [Move {
+                from: Coordinate(0, 5),
+                to: Coordinate(1, 4),
+            }]
+        );
+        assert_eq!(
+            m2,
+            [
+                Move {
+                    from: Coordinate(2, 5),
+                    to: Coordinate(3, 4),
+                },
+                Move {
+                    from: Coordinate(2, 5),
+                    to: Coordinate(1, 4),
+                }
+            ]
+        );
+    }
+
+    #[test]
+    fn legal_moves_black() {
+        let mut engine = GameEngine::new();
+        engine.initialize_pieces();
+        let moves = engine.legal_moves();
+        assert_eq!(
+            moves,
+            [
+                Move {
+                    from: Coordinate(0, 5),
+                    to: Coordinate(1, 4),
+                },
+                Move {
+                    from: Coordinate(2, 5),
+                    to: Coordinate(3, 4),
+                },
+                Move {
+                    from: Coordinate(2, 5),
+                    to: Coordinate(1, 4),
+                },
+                Move {
+                    from: Coordinate(4, 5),
+                    to: Coordinate(5, 4),
+                },
+                Move {
+                    from: Coordinate(4, 5),
+                    to: Coordinate(3, 4),
+                },
+                Move {
+                    from: Coordinate(6, 5),
+                    to: Coordinate(7, 4),
+                },
+                Move {
+                    from: Coordinate(6, 5),
+                    to: Coordinate(5, 4),
+                }
+            ]
+        );
+    }
+
+    #[test]
+    fn legal_moves_white() {
+        let mut engine = GameEngine::new();
+        engine.initialize_pieces();
+        engine.advance_turn();
+        let moves = engine.legal_moves();
+        assert_eq!(
+            moves,
+            [
+                Move {
+                    from: Coordinate(1, 2),
+                    to: Coordinate(0, 3),
+                },
+                Move {
+                    from: Coordinate(1, 2),
+                    to: Coordinate(2, 3),
+                },
+                Move {
+                    from: Coordinate(3, 2),
+                    to: Coordinate(2, 3),
+                },
+                Move {
+                    from: Coordinate(3, 2),
+                    to: Coordinate(4, 3),
+                },
+                Move {
+                    from: Coordinate(5, 2),
+                    to: Coordinate(4, 3),
+                },
+                Move {
+                    from: Coordinate(5, 2),
+                    to: Coordinate(6, 3),
+                },
+                Move {
+                    from: Coordinate(7, 2),
+                    to: Coordinate(6, 3),
+                }
+            ]
+        );
+    }
+
+    #[test]
+    fn jump_targets() {
+        let c1 = Coordinate(3, 3);
+        let targets = c1.jump_targets_from().collect::<Vec<Coordinate>>();
+        assert_eq!(
+            targets,
+            [
+                Coordinate(5, 1),
+                Coordinate(5, 5),
+                Coordinate(1, 1),
+                Coordinate(1, 5)
+            ]
+        );
+    }
+
+    #[test]
+    fn jump_moves_validation() {
+        let mut engine = GameEngine::new();
+        engine.initialize_pieces();
+        engine.board[1][4] = Some(GamePiece::new(PieceColor::White)); // this should be jumpable from 0,5 to 2,3
+        let moves = engine.legal_moves();
+        assert_eq!(
+            moves,
+            [
+                Move {
+                    from: Coordinate(0, 5),
+                    to: Coordinate(2, 3),
+                },
+                Move {
+                    from: Coordinate(2, 5),
+                    to: Coordinate(0, 3)
+                },
+                Move {
+                    from: Coordinate(2, 5),
+                    to: Coordinate(3, 4)
+                },
+                Move {
+                    from: Coordinate(4, 5),
+                    to: Coordinate(5, 4)
+                },
+                Move {
+                    from: Coordinate(4, 5),
+                    to: Coordinate(3, 4)
+                },
+                Move {
+                    from: Coordinate(6, 5),
+                    to: Coordinate(7, 4)
+                },
+                Move {
+                    from: Coordinate(6, 5),
+                    to: Coordinate(5, 4)
+                }
+            ]
+        );
+    }
+
+    #[test]
+    fn test_basic_move() {
+        let mut engine = GameEngine::new();
+        engine.initialize_pieces();
+        let res = engine.move_piece(&Move::new((0, 5), (1, 4)));
+        assert!(res.is_ok());
+
+        let old = engine.board[0][5];
+        let new = engine.board[1][4];
+        assert_eq!(old, None);
+        assert_eq!(
+            new,
+            Some(GamePiece {
+                color: PieceColor::Black,
+                crowned: false
+            })
+        );
+
+        // fail to perform illegal move
+        let res = engine.move_piece(&Move::new((1, 4), (2, 4))); // can't move horiz
+        assert!(!res.is_ok());
+        assert_eq!(engine.board[2][4], None);
+    }
+ */
 }
