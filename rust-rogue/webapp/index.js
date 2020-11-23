09a394956867cf9cf0ecaffb9133175e742a59a5
Joel Hofgren <joel.hofgren@squeed.com>
2020-11-23 23:38:35 +0100

    Hook up a ROT display to wasm game engine and html canvas.

--- rust-rogue/webapp/index.js
+++ rust-rogue/webapp/index.js
@@ -1,4 +1,14 @@
+import {Engine, PlayerCore} from './gen/rust_rogue'
+
 const wasm = import('./gen/rust_rogue.js');
 
-wasm.then(module => console.log("rust-rogue wasm module loaded"))
-    .catch(console.error);
+wasm.then(module => {
+    console.log("wasm module");
+    this.display = new ROT.Display({
+        width: 125, heigth: 40
+    });
+    document.getElementById("rogueCanvas").appendChild(this.display.getContainer());
+
+    this.engine = new Engine(this.display);
+})
+.catch(console.error);
