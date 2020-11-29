b017523bb03337b4fd3368f1d6c82dd7bd8534cd
Joel Hofgren <joel.hofgren@squeed.com>
2020-11-29 19:31:20 +0100

    Render initial board setup.
    Note, seems like there is some mismatch between the square's id coordinates and the wasm modules coordinates.

--- checkers/rust-checkers/demo/game.ts
+++ checkers/rust-checkers/demo/game.ts
@@ -1,3 +1,16 @@
+const rows: number[] = [0,1,2,3,4,5,6,7];
+const cols: number[] = [0,1,2,3,4,5,6,7];
+
+const BLACK = 1;
+const WHITE = 0;
+
+function pieceElement(color: number): Node {
+    let div = document.createElement("div");
+    div.classList.add("piece");
+    div.classList.add(color === BLACK ? "black" : "white");
+    return div;
+}
+
 fetch('./rust_checkers.wasm')
     .then(response => response.arrayBuffer())
     .then(bytes => WebAssembly.instantiate(bytes, {
@@ -10,8 +23,26 @@ fetch('./rust_checkers.wasm')
             }
         }
     }))
-    .then(result => {
+    .then(wasmInstance => {
+        const get_current_turn = (wasmInstance.instance.exports.get_current_turn) as Function;
+        const get_piece = (wasmInstance.instance.exports.get_piece) as Function;
         console.log("imported wasm");
+        console.log("current turn is:", get_current_turn()  === BLACK ? "BLACK" : "WHITE");
+
+        rows.forEach(x => {
+            cols.forEach(y => {
+                const piece = get_piece(y, x);
+                if (piece > 0) {
+                    const square = document.getElementById(`${x},${y}`);
+                    square.appendChild(pieceElement(piece));
+                }
+                
+                //
+                // Add the ondragstart event listener
+                //element.addEventListener("dragstart", dragstart_handler);
+            })
+        });
+
     })
     .catch(error => {
         console.log(error);
