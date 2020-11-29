1482a783b0e98261872f12739fb69f2363c76608
Joel Hofgren <joel.hofgren@squeed.com>
2020-11-29 19:30:51 +0100

    Ensure DOM is loaded before proceeding.

--- checkers/rust-checkers/demo/bootstrap.js
+++ checkers/rust-checkers/demo/bootstrap.js
@@ -1,7 +1,9 @@
-import('./game')
+window.addEventListener('DOMContentLoaded', () => {
+    import('./game')
     .then(wasm => {
         console.log("game module loaded");
     })
     .catch(error => {
         console.error(error);
     });
+});
